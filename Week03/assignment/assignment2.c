bd3d48f564decc15f1c2ab9500e28a11dd4aec2c
Gaon-Choi <choigaon1028@gmail.com>
2021-03-29 03:25:14 +0900

    fix code line

--- Week03/assignment/assignment2.c
+++ Week03/assignment/assignment2.c
@@ -4,7 +4,6 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-
 int main(void)
 {
 	// process id: pid1, pid2
@@ -17,22 +16,22 @@ int main(void)
 
 	pid1 = fork();
 
-    if(pid1 == 0) {
-		execl("./child_1", "", (char*)NULL);
+	if (pid1 == 0) {
+		execl("./child_1", "", (char *)NULL);
 		exit(0);
 	}
 
-    pid2 = fork();
-	if(pid2 == 0) {
-		execl("./child_2", "", (char*)NULL);
+	pid2 = fork();
+	if (pid2 == 0) {
+		execl("./child_2", "", (char *)NULL);
 		waitpid(pid1, &state, 0);
 		exit(0);
 	}
 
-	if(pid1 != 0 && pid2 != 0) {
+	if (pid1 != 0 && pid2 != 0) {
 		waitpid(pid1, &state, 0);
 		waitpid(pid2, &state, 0);
-        printf("Child processes are exit (%d) (%d)\n", (pid1 == 0), (pid2 == 0));
+		printf("Child processes are exit (%d) (%d)\n", (pid1 == 0), (pid2 == 0));
 	}
 	return 0;
 }
