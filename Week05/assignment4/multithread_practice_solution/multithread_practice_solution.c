60b2d3b15a52d618b5e14991899e7a3e151f23a4
Gaon-Choi <choigaon1028@gmail.com>
2021-04-02 00:11:05 +0900

    week05: multithread solution

--- Week05/assignment4/multithread_practice_solution/multithread_practice_solution.c
+++ Week05/assignment4/multithread_practice_solution/multithread_practice_solution.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
+// the number of threads
 #define ARGUMENT_NUMBER 20
 
 long long result = 0;
+
 void *ThreadFunc(void *n) {
     long long i;
     long long number = *((long long *)n);
     printf("number = %lld\n", number);
-    for(i = 0; i < 25000000; i++) {
+    for(i = 0; i < 25000000; ++i) {
+        // add number to result_component of corresponding thread
         result += number;
     }
 }
 
 int main(void) {
     long long argument[ARGUMENT_NUMBER];
+    pthread_t threadID[ARGUMENT_NUMBER];
     long long i;
 
-    for(i = 0; i < ARGUMENT_NUMBER; i++) {
+    for(i = 0; i < ARGUMENT_NUMBER; ++i) {
         argument[i] = i;
     }
 
-    for(i = 0; i < ARGUMENT_NUMBER; i++) {
-        ThreadFunc((void*)&argument[i]);
+    // create new threads
+    // total num : ARGUMENT_NUMBER
+    for(i = 0; i < ARGUMENT_NUMBER; ++i) {
+        pthread_create(&(threadID[i]), NULL, ThreadFunc, (void*)(&argument[i]));
+    }
+
+    // wait for the termination of a specific thread
+    // return immediately if thread has already ended
+    for(i = 0; i < ARGUMENT_NUMBER; ++i) {
+        pthread_join(threadID[i], NULL);
     }
 
     printf("result = %lld\n", result);
