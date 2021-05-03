f6a1a546d34ed54c5ddb027e73e29fd0e5a55f33
Gaon Choi <choigaon1028@gmail.com>
2021-05-04 08:07:18 +0900

    week07: assignment_A

--- Week07/assignment/assignment_A/first_rw_problem.c
+++ Week07/assignment/assignment_A/first_rw_problem.c
@@ -15,26 +15,94 @@
 
 #define COUNTING_NUMBER 1000000
 
-void writer();
+#define WRITER_NUM 5
+#define READER_NUM 2
+
+void writer(void *n);
 void reader();
 
 sem_t S;
 sem_t wrt;
 
-void writer() {
+int cur_writer = 0;
+int cur_count = 0;
+int readcount = 0;
+int writer_access_count[WRITER_NUM];
+
+void writer(void *n) {
     int i;
     for(i = 0; i < COUNTING_NUMBER; i++) {
-        // blank
+        usleep(100000);
+        // lock
+        sem_wait(&wrt);
+
+        cur_writer *= *((int*)n);
+        cur_count = writer_access_count[cur_writer]++;
+        cur_count %= 100;
+
+        // unlock
+        sem_post(&wrt);
     }
 }
 
 void reader() {
     int i;
     for(i = 0; i < COUNTING_NUMBER; i++) {
-        // blank
+        usleep(30000);
+        // lock
+        sem_wait(&S);
+        readcount++;
+        if(readcount == 1) {
+            sem_wait(&wrt);
+        }
+        sem_post(&S);
+
+        /* critical section */
+        printf("The most recent writer id : \t[%d], count : \t[%d]\n", cur_writer, cur_count);
+
+        // unlock
+        sem_wait(&S);
+        readcount--;
+        if(readcount == 0) {
+            sem_post(&wrt);
+        }
+        sem_post(&S);
     }
 }
 
 int main() {
+    int argument[5] = { 0, 1, 2, 3, 4 };
+    int i;
 
+    sem_init(&S, 0, 1);     // set semaphore S to 1
+    sem_init(&wrt, 0, 1);   // set semaphore wrt to 1
+
+    pthread_t writer_thread[WRITER_NUM];
+    pthread_t reader_thread[READER_NUM];
+
+    for(i = 0; i < WRITER_NUM; ++i) {
+        // create writer threads
+        pthread_create(&(writer_thread[i]), NULL, (void*)&writer, (void*)&argument[i]);
+    }
+
+    for(i = 0; i < READER_NUM; ++i) {
+        // create reader threads
+        pthread_create(&(reader_thread[i]), NULL, (void*)&reader, NULL);
+    }
+
+    printf("Waiting for threads to be finished\n");
+
+    for(i = 0; i < WRITER_NUM; ++i) {
+        pthread_join(writer_thread[i], NULL);
+    }
+
+    for(i = 0; i < READER_NUM; ++i) {
+        pthread_join(reader_thread[i], NULL);
+    }
+
+    // destroy semaphores : S, wrt
+    sem_destroy(&S);
+    sem_destroy(&wrt);
+
+    return 0;
 }
